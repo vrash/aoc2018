52ff8b321c80e47eb4ac133ffa09afff7629c525
Vrashabh Irde <vrashabh86@gmail.com>
2018-12-10 15:14:46 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,28 @@
+from collections import deque
+import fileinput
+import re
+import time
+
+def winningScore(players, lastmarble):
+    #problem struct is a double ended queue
+    d = deque([0])
+    ans = [0] * players
+    #print ans
+    for i in range(1, lastmarble + 1):
+        if i % 23 == 0:
+            d.rotate(7)
+            update = i + d.pop()
+            ans[i % players] += update
+            d.rotate(-1)
+        else:
+            d.rotate(-1)
+            d.append(i)
+    return max(ans)
+
+#1
+print(winningScore(405, 71700))
+#2
+start = time.clock()
+print(winningScore(405, 7170000))
+#time in seconds for part 2
+print time.clock() - start
